a3ea0862d31526b987effff2aee0c9ffa18688ee
chandu <chandrasr.palle@gmail.com>
2018-03-22 16:33:33 +0100

    assignment answers updated

--- step2.js
+++ step2.js
@@ -1,1 +1,21 @@
 console.log("step 2");
+
+$(document).ready(function()
+    {
+            $("#step2").click(function(){
+                if($("h2").text()=="Green")
+                {
+                $("h2").text("Red");
+                $("h2").css("background","red");
+                $("h2").fadeOut(2000);
+                }
+                else if($("h2").text()=="Red")
+                {
+                    $("h2").text("Green");
+                    $("h2").css("background","Green");
+                    $("h2").fadeIn(2000);
+                }
+            });  
+        }
+
+);
