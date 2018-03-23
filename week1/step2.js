a82156bfc9c44cea950684919e6250dd9f002f03
chandu <chandrasr.palle@gmail.com>
2018-03-23 14:36:11 +0100

    containers added and step2 updated

--- week1/step2.js
+++ week1/step2.js
@@ -1,22 +1,46 @@
 console.log("step 2");
 
 $(document).ready(function()
-    {
-            $("#step2").click(function(){
-                // if($("h2").text()=="Green")
-                // {
-                // $("h2").text("Red");
-                // $("h2").css("background","red");
-                // $("h2").fadeOut(2000);
-                // }
-                // else if($("h2").text()=="Red")
-                // {
-                //     $("h2").text("Green");
-                //     $("h2").css("background","Green");
-                //     $("h2").fadeIn(2000);
-                // }
-                $("h2").fadeToggle();
-            });  
-        }
+    {   
+        // hiding all containers
+        $("#step1-container").hide();
+        $("#step2-container").hide();
 
+        // on click response functionalities
+            $("#fadetoggle-button").click(function(){
+                $("#fadetoggle-area").fadeToggle();
+            });  
+        
+            //image fadein and fadeout
+            $("#fadeinbutton").click(function(){
+                $("img").fadeIn(2000);
+            });
+            $("#fadeoutbutton").click(function(){
+                $("img").fadeOut(2000);
+            });
+
+            //slidetoggle for containers
+            $("#step1-heading").click(function(){
+                $("#step1-container").slideToggle();
+            });
+            $("#step2-heading").click(function(){
+                $("#step2-container").slideToggle();
+            });
+            
+            // creating fade toggle with single button using fadein fadeout
+            $("#fadeinout-button").click(function(){
+                if($("#fadeinout-text").text()=="Welcome")
+                {
+                $("#fadeinout-text").text("Bye");
+                $("#fadeinout-text").fadeOut(2000);
+                }
+                else if($("#fadeinout-text").text()=="Bye")
+                {
+                    $("#fadeinout-text").text("Welcome");
+                    $("#fadeinout-text").fadeIn(2000);
+                }
+
+            });
+            
+    }
 );
